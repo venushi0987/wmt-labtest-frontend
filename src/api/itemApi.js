74bc5eda5772a50f6b5dc2fce7055d6d4443f7dc
IT24103206 <it24103206@my.sliit.lk>
2026-04-29 16:43:40 +0530

    Update front end url

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://wmt-labtest-backend-production-8a2e.up.railway.app/api",
+  baseURL: "https://wmt-labtest-backend-production-8a2e.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
